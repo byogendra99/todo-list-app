5274dce5df4b7eb2973b61fc16a513a158f537fe
Yogendra Baral <byogendra99@gmail.com>
2020-08-25 19:25:13 -0500

    complete commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import InputForm from './Components/InputForm';
+import TodoList from './Components/TodoList';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { addCompleted, updateTodo, clearCompleted, addTodo } from './actions';
 
-function App() {
+function App(props) {
+  const [item, setItem] = useState('');
+  const [index, setIndex] = useState('');
+
+  const handleEdit = (task, i) => {
+    setItem(task);
+    setIndex(i);
+  }
+
+  const handleChange = (e) => {
+    const input = e.target.value;
+    setItem(input);
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    props.addTodo(item, index);
+    setItem('');
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <InputForm handleChange={handleChange} handleSubmit={handleSubmit} item={item} />
+      <TodoList todoList={props.todoList} list="todoList" addCompleted={props.addCompleted} handleEdit={handleEdit} checked={false} />
+      <TodoList todoList={props.completedList} list="Completed List" updateTodo={props.updateTodo} clearCompleted={props.clearCompleted} checked={true} />
     </div>
   );
 }
 
-export default App;
+export default connect(
+  state => ({ todoList: state.todoList, completedList: state.completedList }),
+  dispatch => ({
+    addCompleted: bindActionCreators(addCompleted, dispatch),
+    updateTodo: bindActionCreators(updateTodo, dispatch),
+    clearCompleted: bindActionCreators(clearCompleted, dispatch),
+    addTodo: bindActionCreators(addTodo, dispatch)
+  })
+)(App);
